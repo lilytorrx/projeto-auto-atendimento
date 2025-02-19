13dc33aa8b56452d903d5a4fad14e2615c58127a
lilytorrx <repositoriolidia@gmail.com>
2025-02-19 15:13:16 -0300

    fix: arrumei o banco de dados

---
+++ prisma/migrations/20250219181100_add_database_english/migration.sql
@@ -0,0 +1,139 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Pedido` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Produto` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Restaurante` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `categoriaMenu` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `pedidoProdutos` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'IN_PREPARATION', 'FINISHED');
+
+-- CreateEnum
+CREATE TYPE "ConsumptionMethod" AS ENUM ('TAKEAWAY', 'DINE_IN');
+
+-- DropForeignKey
+ALTER TABLE "Pedido" DROP CONSTRAINT "Pedido_idRestaurante_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Produto" DROP CONSTRAINT "Produto_idCategoriaMenu_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Produto" DROP CONSTRAINT "Produto_idRestaurante_fkey";
+
+-- DropForeignKey
+ALTER TABLE "categoriaMenu" DROP CONSTRAINT "categoriaMenu_idRestaurante_fkey";
+
+-- DropForeignKey
+ALTER TABLE "pedidoProdutos" DROP CONSTRAINT "pedidoProdutos_idPedido_fkey";
+
+-- DropForeignKey
+ALTER TABLE "pedidoProdutos" DROP CONSTRAINT "pedidoProdutos_idProduto_fkey";
+
+-- DropTable
+DROP TABLE "Pedido";
+
+-- DropTable
+DROP TABLE "Produto";
+
+-- DropTable
+DROP TABLE "Restaurante";
+
+-- DropTable
+DROP TABLE "categoriaMenu";
+
+-- DropTable
+DROP TABLE "pedidoProdutos";
+
+-- DropEnum
+DROP TYPE "Metodo";
+
+-- DropEnum
+DROP TYPE "Status";
+
+-- CreateTable
+CREATE TABLE "Restaurant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "avatarImageUrl" TEXT NOT NULL,
+    "coverImageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Restaurant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuCategory" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "restaurantId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MenuCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "ingredients" TEXT[],
+    "restaurantId" TEXT NOT NULL,
+    "menuCategoryId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "status" "OrderStatus" NOT NULL,
+    "consumptionMethod" "ConsumptionMethod" NOT NULL,
+    "restaurantId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProduct" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OrderProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "MenuCategory" ADD CONSTRAINT "MenuCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_menuCategoryId_fkey" FOREIGN KEY ("menuCategoryId") REFERENCES "MenuCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
