f6edd79bd53fead849157a6037d29f60857078a9
lilytorrx <repositoriolidia@gmail.com>
2025-03-07 22:44:40 -0300

    chore: atualizar HomePage e variáveis globais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const HomePage = () => {
-  return (
-    <h1>Hello World</h1>
-  );
+    return (
+        <div className="p-5 border border-red-500 rounded-xl">
+            <h1 className="text-red-500">Página de produtos</h1>
+            <Button>Lily Donalds</Button>
+            <Input placeholder="ALGUÉM ME TRUCIDA POR FAVOR"></Input>
+        </div>
+    );
 }
 
 export default HomePage;
